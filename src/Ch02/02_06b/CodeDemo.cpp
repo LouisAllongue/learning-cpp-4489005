82686671b0a1bff3dda71e3ea2a23adb654c9254
Louwi <140532033+LouisAllongue@users.noreply.github.com>
2025-07-31 09:34:15 +0000

    exo

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,8 +3,23 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+#define CAPACITY 50000
+#define DEBUG
 
 int main(){
+
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG
+    std::cout << "[About to perform an addition]" << std::endl;
+#endif
+    large += small;
+
+    std::cout << "Large is  " << large << std::endl;
+
     
     std::cout << std::endl << std::endl;
     return (0);
