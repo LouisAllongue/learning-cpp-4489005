32629a2334f8b651cff715c5572c43c057c0e732
Louwi <140532033+LouisAllongue@users.noreply.github.com>
2025-08-01 09:06:32 +0000

    ch2

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,6 +6,18 @@
 #include <cstdint>
 
 int main(){
+    float flt;
+    int32_t sgn;
+    uint32_t unsgn;
+
+    flt = -7.66;
+    sgn = flt;
+    unsgn = sgn;
+
+    std::cout << "flt is " << flt << std::endl;
+    std::cout << "sgn is " << sgn << std::endl;
+    std::cout << "unsgn is " << (int32_t) unsgn << std::endl;
+
     
     std::cout << std::endl << std::endl;
     return (0);
