14037a5a92395861302504a3789af0a769c56831
Louwi <140532033+LouisAllongue@users.noreply.github.com>
2025-08-04 07:41:03 +0000

    ch3

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -7,6 +7,44 @@
 
 int main(){
     
+    struct{
+        std::string name;
+        int age;
+        float height;
+
+        float heightInMeters() const {
+            return height /100.0f;
+        } 
+
+
+    }person;
+
+    person.name ="Louwi";
+    person.age = 25;
+    person.height = 180;
+
+    std::cout << "Name : " << person.name << std::endl;
+    std::cout << "Age: " << person.age << std::endl;
+    std::cout << "Height: " << person.height << " cm" << std::endl;
+    std::cout << "heightInMeters: " << person.heightInMeters() << "m" << std::endl;
+
+    std::cout << std::endl;
+
+    person.name = "Haize";
+    person.age = 24;
+    person.height = 170;
+
+    std::cout << "Name : " << person.name << std::endl;
+    std::cout << "Age: " << person.age << std::endl;
+    std::cout << "Height: " << person.height << " cm" << std::endl;
+    std::cout << "heightInMeters: " << person.heightInMeters() << "m" << std::endl;
+
+
+
+
+
+
+    
     std::cout << std::endl << std::endl;
     return (0);
 }
